2c713d8ae7a8a88f662130768de00674e91bd16c
Rayyan <rayyanaslam16@gmail.com>
2024-05-01 13:23:08 +0530

    Render component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import JobsList from "./components/JobsList";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="container mx-auto py-8 ">
+        <h1 className="text-white text-3xl font-bold mb-4">Job Listings</h1>
+        <JobsList />
+      </div>
+    </>
+  );
 }
 
 export default App;
